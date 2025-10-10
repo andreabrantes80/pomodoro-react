bad2d2ffbc7922068776d85781c3842f8174a5a4
andreabrantes80 <meloabrantes@gmail.com>
2025-10-09 23:52:51 -0300

    correção dos tempos do app

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App(): JSX.Element {
   return (
     <div className="container">
       <PomodoroTime
-        pomodoroTime={10}
-        shortRestTime={3}
-        longRestTime={9}
+        pomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
         cycles={4}
       />
     </div>
